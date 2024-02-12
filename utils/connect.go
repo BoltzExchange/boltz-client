14513784219a89e2779d901e10da159124dbd6b7
jackstar12 <jkranawetter05@gmail.com>
2024-02-12 20:43:45 +0100

    feat: improve node connection selection

--- utils/connect.go
+++ utils/connect.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"net"
 	"strings"
 
 	"github.com/BoltzExchange/boltz-client/boltz"
@@ -10,29 +11,28 @@ import (
 	"github.com/BoltzExchange/boltz-client/logger"
 )
 
-func connect(node lightning.LightningNode, peer boltz.NodeInfo) error {
-	var uri string
-	for _, current := range peer.Uris {
-		uri = current
-		// prefer clearnet
-		if !strings.Contains(current, "onion") {
-			break
+func searchUri(network string, uris []string) string {
+	for _, uri := range uris {
+		split := strings.Split(uri, "@")
+		if len(split) < 2 {
+			continue
+		}
+		split = strings.Split(split[1], ":")
+		if len(split) < 2 {
+			continue
+		}
+		// remove the port
+		split = split[:len(split)-1]
+		if network != "tor" {
+			_, err := net.ResolveIPAddr(network, strings.Join(split, ":"))
+			if err == nil {
+				return uri
+			}
+		} else if strings.Contains(uri, ".onion") {
+			return uri
 		}
 	}
-	if uri == "" {
-		return fmt.Errorf("no uri for peer: %s", peer.PublicKey)
-	}
-	err := node.ConnectPeer(uri)
-	if err == nil {
-		logger.Infof("Connected to Boltz node: %s", uri)
-	} else if strings.Contains(err.Error(), "already connected to peer") {
-		logger.Infof("Already connected to Boltz node: %s", uri)
-		err = nil
-	} else {
-		err = fmt.Errorf("could not connect to boltz node %s: %w", uri, err)
-	}
-
-	return err
+	return ""
 }
 
 func ConnectBoltz(lightning lightning.LightningNode, boltz *boltz.Boltz) error {
@@ -49,8 +49,27 @@ func ConnectBoltz(lightning lightning.LightningNode, boltz *boltz.Boltz) error {
 	}
 
 	for name, node := range nodesForSymbol {
-		if err := connect(lightning, node); err != nil {
-			logger.Errorf("Could not connect to node %s: %s", name, err)
+		fullName := fmt.Sprintf("%s (%s)", name, node.PublicKey)
+		if len(node.Uris) == 0 {
+			logger.Warnf("no uris for node %s", fullName)
+			continue
+		}
+		for _, network := range []string{"ip4", "ip6", "tor"} {
+			uri := searchUri(network, node.Uris)
+			if uri == "" {
+				logger.Infof("no %s uri for node %s", network, fullName)
+				continue
+			}
+			err := lightning.ConnectPeer(uri)
+			if err == nil || strings.Contains(err.Error(), "already connected to peer") {
+				err = nil
+				break
+			}
+		}
+		if err == nil {
+			logger.Infof("Connected to Boltz node %s", fullName)
+		} else {
+			logger.Errorf("could not connect to boltz node %s: %v", fullName, err)
 		}
 	}
 	return err
