c274f3b5cbf967a473b08680a8bd5383f58e11ac
jackstar12 <jkranawetter05@gmail.com>
2024-07-08 00:02:20 +0200

    chore: cleanup

--- database/swap.go
+++ database/swap.go
@@ -388,14 +388,14 @@ func (database *Database) SetSwapRefundTransactionId(swap *Swap, refundTransacti
 func (database *Database) SetSwapRefundAddress(swap *Swap, refundAddress string) error {
 	swap.RefundAddress = refundAddress
 
-	_, err := database.Exec("UPDATE swaps SET refundAddress = ?  WHERE id = ?", refundAddress, swap.Id)
+	_, err := database.Exec("UPDATE swaps SET refundAddress = ? WHERE id = ?", refundAddress, swap.Id)
 	return err
 }
 
 func (database *Database) SetSwapRefundWallet(swap *Swap, walletId Id) error {
 	swap.WalletId = &walletId
 
-	_, err := database.Exec("UPDATE swaps SET walletId = ?  WHERE id = ?", walletId, swap.Id)
+	_, err := database.Exec("UPDATE swaps SET walletId = ? WHERE id = ?", walletId, swap.Id)
 	return err
 }
 
