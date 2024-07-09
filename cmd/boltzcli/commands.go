c5359589b7a45987cb40938f59b54d75dbe0beae
Kilian <19181985+kilrau@users.noreply.github.com>
2024-07-09 10:38:24 +0200

    Update cmd/boltzcli/commands.go
    Co-authored-by: jackstar12 <62219658+jackstar12@users.noreply.github.com>

--- cmd/boltzcli/commands.go
+++ cmd/boltzcli/commands.go
@@ -722,7 +722,7 @@ func autoSwapLightningSetup(ctx *cli.Context) error {
 
 	var balanceType string
 	prompt = &survey.Select{
-		Message: "How do you want to specify values?",
+		Message: "How do you want to specify balance values?",
 		Options: []string{"percentage", "sats"},
 	}
 	if err := survey.AskOne(prompt, &balanceType); err != nil {
