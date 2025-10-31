BoltzExchange/boltz-client

commit 919ea69e1be892944c4c3cb9c985e1cbcaf2fb74

refactor: default electrum config
its easier to check for existance of electrum config if its a pointer.
apply the default regtest servers only in the wallets and not in the
whole config because we dont actually wanna rely on them in integration
tests since they are a bit slow.

internal/onchain/wallet/wallet.go
@@ -290,10 +290,13 @@ func (wallet *Wallet) Connect() error {
 		"gap_limit":     GapLimit,
 		"discount_fees": true,
 	}
-	var electrum onchain.ElectrumOptions
+	var electrum *onchain.ElectrumOptions
 	switch wallet.Currency {
 	case boltz.CurrencyBtc:
 		electrum = config.Electrum.Btc
+		if electrum == nil {
+			electrum = onchain.RegtestElectrumConfig.Btc
+		}
 		switch config.Network {
 		case boltz.MainNet:
 			params["name"] = "electrum-mainnet"
@@ -306,6 +309,9 @@ func (wallet *Wallet) Connect() error {
 		}
 	case boltz.CurrencyLiquid:
 		electrum = config.Electrum.Liquid
+		if electrum == nil {
+			electrum = onchain.RegtestElectrumConfig.Liquid
+		}
 		switch config.Network {
 		case boltz.MainNet:
 			params["name"] = "electrum-liquid"
@@ -319,7 +325,7 @@ func (wallet *Wallet) Connect() error {
 	default:
 		return errors.New("unknown currency")
 	}
-	if electrum.Url != "" {
+	if electrum != nil {
 		params["electrum_url"] = electrum.Url
 		params["electrum_tls"] = electrum.SSL
 	}

internal/config/config.go
@@ -98,10 +98,14 @@ type Config struct {
 }
 
 func (c *Config) Electrum() onchain.ElectrumConfig {
-	return onchain.ElectrumConfig{
-		Btc:    onchain.ElectrumOptions{Url: c.ElectrumUrl, SSL: c.ElectrumSSL},
-		Liquid: onchain.ElectrumOptions{Url: c.ElectrumLiquidUrl, SSL: c.ElectrumLiquidSSL},
+	config := onchain.ElectrumConfig{}
+	if c.ElectrumUrl != "" {
+		config.Btc = &onchain.ElectrumOptions{Url: c.ElectrumUrl, SSL: c.ElectrumSSL}
 	}
+	if c.ElectrumLiquidUrl != "" {
+		config.Liquid = &onchain.ElectrumOptions{Url: c.ElectrumLiquidUrl, SSL: c.ElectrumLiquidSSL}
+	}
+	return config
 }
 
 func LoadConfig(dataDir string) (*Config, error) {

internal/electrum/electrum_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func client(t *testing.T) *electrum.Client {
-	client, err := electrum.NewClient(onchain.RegtestElectrumConfig.Btc)
+	client, err := electrum.NewClient(*onchain.RegtestElectrumConfig.Btc)
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		client.Disconnect()

internal/onchain/onchain.go
@@ -53,13 +53,13 @@ type ElectrumOptions struct {
 }
 
 type ElectrumConfig struct {
-	Btc    ElectrumOptions
-	Liquid ElectrumOptions
+	Btc    *ElectrumOptions
+	Liquid *ElectrumOptions
 }
 
 var RegtestElectrumConfig = ElectrumConfig{
-	Btc:    ElectrumOptions{Url: "localhost:19001"},
-	Liquid: ElectrumOptions{Url: "localhost:19002"},
+	Btc:    &ElectrumOptions{Url: "localhost:19001"},
+	Liquid: &ElectrumOptions{Url: "localhost:19002"},
 }
 
 const DefaultWalletSyncInterval = 60 * time.Second

internal/rpcserver/server.go
@@ -237,10 +237,7 @@ func (server *routedBoltzServer) start(cfg *config.Config) (err error) {
 		DataDir:       cfg.DataDir + "/liquid-wallet",
 		ChainProvider: server.onchain.Liquid.Chain,
 		Persister:     database.NewWalletPersister(server.database),
-	}
-	electrumConfig := cfg.Electrum()
-	if electrumConfig.Liquid.Url != "" {
-		liquidConfig.Electrum = &electrumConfig.Liquid
+		Electrum:      cfg.Electrum().Liquid,
 	}
 	server.walletBackends[boltz.CurrencyLiquid], err = liquid_wallet.NewBackend(liquidConfig)
 	if err != nil {
@@ -445,9 +442,6 @@ func initOnchain(cfg *config.Config, boltzApi *boltz.Api, network *boltz.Network
 	chain.Init()
 
 	electrumConfig := cfg.Electrum()
-	if network == boltz.Regtest && electrumConfig.Btc.Url == "" && electrumConfig.Liquid.Url == "" {
-		electrumConfig = onchain.RegtestElectrumConfig
-	}
 
 	if !wallet.Initialized() {
 		if cfg.AutoConsolidateThreshold == nil {
@@ -466,17 +460,17 @@ func initOnchain(cfg *config.Config, boltzApi *boltz.Api, network *boltz.Network
 		}
 	}
 
-	if electrumConfig.Btc.Url != "" {
+	if electrumConfig.Btc != nil {
 		logger.Info("Using configured Electrum BTC RPC: " + electrumConfig.Btc.Url)
-		client, err := electrum.NewClient(electrumConfig.Btc)
+		client, err := electrum.NewClient(*electrumConfig.Btc)
 		if err != nil {
 			return nil, fmt.Errorf("could not connect to electrum: %v", err)
 		}
 		chain.Btc.Chain = client
 	}
-	if electrumConfig.Liquid.Url != "" {
+	if electrumConfig.Liquid != nil {
 		logger.Info("Using configured Electrum Liquid RPC: " + electrumConfig.Liquid.Url)
-		client, err := electrum.NewClient(electrumConfig.Liquid)
+		client, err := electrum.NewClient(*electrumConfig.Liquid)
 		if err != nil {
 			return nil, fmt.Errorf("could not connect to electrum: %v", err)
 		}

commit 944f09cc5b1638883584f13fbfbc2ca361da112d

fix(gdk): dont apply default regtest electrum on every network
regression was introduce in:
919ea69e1be892944c4c3cb9c985e1cbcaf2fb74

internal/onchain/wallet/wallet.go
@@ -294,7 +294,7 @@ func (wallet *Wallet) Connect() error {
 	switch wallet.Currency {
 	case boltz.CurrencyBtc:
 		electrum = config.Electrum.Btc
-		if electrum == nil {
+		if electrum == nil && config.Network == boltz.Regtest {
 			electrum = onchain.RegtestElectrumConfig.Btc
 		}
 		switch config.Network {
@@ -309,7 +309,7 @@ func (wallet *Wallet) Connect() error {
 		}
 	case boltz.CurrencyLiquid:
 		electrum = config.Electrum.Liquid
-		if electrum == nil {
+		if electrum == nil && config.Network == boltz.Regtest {
 			electrum = onchain.RegtestElectrumConfig.Liquid
 		}
 		switch config.Network {